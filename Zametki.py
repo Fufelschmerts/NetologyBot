f402837575008cb9a404c6d98c703c82770d30bf
Ivan Malinovsky <vanekmf@gmail.com>
2023-11-21 15:40:03 +0400

    HW 1 complete

---
+++ Zametki.py
@@ -0,0 +1,23 @@
+# Словари - тестирую
+
+dictionary = {
+    'car': 'jeep',
+    'city': 'Batumi'
+}
+
+# print(dictionary)
+# print(dictionary['car'])
+
+cars = {
+    'Ford': ['Focus', "Scorpio", 'Mondeo'],
+    'Lexus': ['GLS', 'S300', 'Turbo']
+}
+
+
+# Добавление ключа и его значений
+cars['Porshe'] = ['911', 'Cayenne', 'Butilka']
+
+# Добавление значе®ний в ключ
+cars['Ford'].append('Fufel')
+
+print(cars['Ford'])
