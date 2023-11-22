d3fca2c5e4bf01525614aa29eab48156dbcce7be
Ivan Malinovsky <vanekmf@gmail.com>
2023-11-23 02:53:03 +0400

    HW 2 complete

--- main.py
+++ main.py
@@ -2,11 +2,60 @@
 
 # Практика 1. Запрос данных у пользователей.
 
-todo_dic = {}  # Создаем пустой словарь
+HELP = '''
+help - показать инфу о Боте
+add - добавить задачу в список (запросить у пользователя)
+show - показать добавленные задачи
+exit - остановить Бота
+'''
 
-for i in range(3):  # Запрашиваем данные у пользователя 3 раза
-    data = input("Введите дату: ")
-    todo = input("Введите задачу: ")
-    todo_dic[data] = todo  # Записываем данные в словарь
+today = {}
+tomorrow = {}
+other = {}
 
-print(todo_dic)
+run = True
+
+while run:
+    command = input('Введите команду: ')
+    if command == 'help':
+        print(HELP)
+    elif command == 'add':
+        data = input("Введите дату: ")
+        task = input("Введите задачу: ")
+        if data == 'Сегодня':
+            today[data] = task
+        elif data.lower() == 'завтра':
+            tomorrow[data] = task
+        else:
+            other[data] = task
+        print(f'Задача "{task}" добавлена')
+    elif command == 'show':
+        if today == {}:
+            print('Задач на сегодня нет')
+        else:
+            print('Задачи на сегодня: ', today)
+        if tomorrow == {}:
+            print('Задач на завтра нет')
+        else:
+            print('Задачи на завтра: ', tomorrow)
+        if other == {}:
+            print('Задач на другие даты нет')
+        else:
+            print('Другие даты: ', other)
+    elif command == 'exit':
+        print('Спасибо за использование! До свидания!')
+        break
+    else:
+        print('Неизвестная команда')
+        break
+
+print('Заходите ещё!')
+
+# todo_dic = {}  # Создаем пустой словарь
+#
+# for i in range(3):  # Запрашиваем данные у пользователя 3 раза
+#     data = input("Введите дату: ")
+#     todo = input("Введите задачу: ")
+#     todo_dic[data] = todo  # Записываем данные в словарь
+#
+# print(todo_dic)
